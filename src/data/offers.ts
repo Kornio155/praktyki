c190e0931f2f3bd650e2a22254b792dff5dfadb3
Mk <maciej.krzywda@cyfroweszkoly.pl>
2026-05-14 11:30:46 +0200

    zmiany wyglądu ofert na bardziej "prostokątne" wypełnienie wymogów pana, zniszczenia własnej psychiki jako dodatek

--- src/data/offers.ts
+++ src/data/offers.ts
@@ -26,8 +26,25 @@ export const offers = [
             'Niekonwencjonalne ćwiczenia z dziedzin streetmovement, animaln movement lub gimnastyki pomogą ci się rozwinąć, i dodadzą nutkę świetnej zabawy!'
     },
     {
+        title: 'Plany treningowe',
+        desc: 'plan pod kątem twoich potrzeb',
+        details: 'Najlepszy plan treningowy to taki, który jest dopasowany do Twoich możliwości treningowych!  Nie każdy ma możliwośc trenowania 4-5 razy w tygodniu po dwie godziny. Dlatego przygotowuję plany ćwiczeń uwględniające to, że nie mieszkasz na siłowni i masz wiele innych obowiązków w życiu prócz cwiczeń.  \n'
+    },
+    {
+        title: 'treningi dla dwojga / w małych grupach',
+        desc: 'druga osoba to najlepsza motywacja i lepsza cena;)!',
+        details: 'Wszystkie rodzaje treningów mogą być również realizowane w parach lub małych grupach. To świetne rozwiązanie, jeśli chcesz ćwiczyć z partnerem, znajomymi lub rodziną!\n' +
+            'To nie tylko dodatkowa motywacja, ale też przyjemniejsza forma wspólnego spędzania czasu.\n' +
+            'Taki typu trening wciąż zachowuje indywidualne podejście i wysoką jakość, ale jednocześnie pozwala obniżyć koszt na osobę.\n' +
+            'Skontaktuj się ze mną, a dobierzemy najlepszą formę treningu dla Ciebie i Twojej grupy'
+    },
+    {
         title: 'Gry i zabawy',
         desc: 'aktywność, integracja, dobra zabawa!',
         details: 'Jeśli szukasz aktywnej formy zajęć dla grup zapraszam do kontaktu! Prowadzę szereg gier i zabaw ruchowych, które zapewnią ruch, integrację i pozytywne doświadczenia w dobrej atmosferze w każdej ekipie!'
+    },
+    {
+        title: 'personal training sessions in English',
+        details: 'I provide personal training sessions in English – perfect for international clients living in Poland or anyone who feels more comfortable communicating in English. Feel free to reach out and book your session – I’ll be happy to help you achieve your fitness goals.For more information, feel free to contact me using any of the available options on my website or by clicking the button below See you on trainings!'
     }
 ]
