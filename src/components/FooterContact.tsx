444fadc5758194b18e9fa61ecf184bb6e87ba5fe
Mk <maciej.krzywda@cyfroweszkoly.pl>
2026-05-25 14:47:38 +0200

    czas się spędzało miło, wielu zmian nadszedł tłum, piwo kiedyś mi się śniło, a teraz idę po rum

--- src/components/FooterContact.tsx
+++ src/components/FooterContact.tsx
@@ -4,57 +4,23 @@ const FooterContact: React.FC = () => {
     return (
         <section className="footer-contact">
 
+            <button
+                className="scroll-top-btn"
+                onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+            >
+                ↑ Na górę ↑
+            </button>
+
             <div className="footer-contact-row">
+                <h2 className="footer-title">Twórcy strony</h2>
 
-                {/* LOKALIZACJA */}
-                <div className="footer-contact-box">
-
-
-
-                    <div className="footer-content">
-                        <h3>Maciej Krzywda</h3>
-                        <p>debil z powołania</p>
-                        <div className="line"></div>
-
-                        <p className="muted">programista z wyboru</p>
-                    </div>
-
+                <div className="footer-list">
+                    <p>Maciej Krzywda</p>
+                    <p>Patryk Witkowski</p>
+                    <p>Kornel Janiuk</p>
                 </div>
+            </div>
 
-                {/* GODZINY */}
-                <div className="footer-contact-box">
-
-
-
-                    <div className="footer-content">
-                        <h3>Patryk Witkowski</h3>
-                        <p>jedyna osoba która wie co robi</p>
-
-                        <div className="line"></div>
-
-                        <p className="muted">w pokoju pełnym gałganów</p>
-                    </div>
-
-                </div>
-
-                {/* KONTAKT */}
-                <div className="footer-contact-box">
-
-
-
-                    <div className="footer-content">
-                        <h3>Kornel Janiuk</h3>
-
-                        <p>nie znam chłopa</p>
-
-                        <div className="line"></div>
-
-                        <p className="muted">nie no naprawde nigdy chłopa nawet nie widziałem</p>
-                        </div>
-
-                    </div>
-
-                </div>
         </section>
     );
 };
