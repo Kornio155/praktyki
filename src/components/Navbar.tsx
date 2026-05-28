35d9d52e88547ef47b45fecec42d3ed7676c5178
PatZ1Z <patryk.witkowski@cyfroweszkoly.pl>
2026-05-28 12:04:29 +0200

    Poprawa responsywności hero oraz navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,10 +8,11 @@ import '../styleSheets/NavBar.css'
 const Navbar: React.FC = () => {
     const [isModalOpen, setIsModalOpen] = useState(false);
 
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
         <>
             <nav className="navbar">
-
 
                 <Link
                     to="/"
@@ -21,18 +22,34 @@ const Navbar: React.FC = () => {
                     <img src={logo} />
                 </Link>
 
-                <ul className="navbar-links">
+                <ul className={`navbar-links ${menuOpen ? "active" : ""}`}>
                     <li><a href="/#contact">Kontakt</a></li>
                     <li><a href="/#offer">Oferta</a></li>
                     <li><a href="/#before-after">Efekty</a></li>
                     <li><Link to="/about">O mnie</Link></li>
+
+                    <button
+                        className="navbar-btn mobile-btn"
+                        onClick={() => setIsModalOpen(true)}
+                    >
+                        Poproś o kontakt
+                    </button>
                 </ul>
 
                 <button
-                    className="navbar-btn"
+                    className="navbar-btn desktop-btn"
                     onClick={() => setIsModalOpen(true)}
                 >
                     Poproś o kontakt
+                </button>
+
+                <button
+                    className={`hamburger ${menuOpen ? "open" : ""}`}
+                    onClick={() => setMenuOpen(!menuOpen)}
+                >
+                    <span></span>
+                    <span></span>
+                    <span></span>
                 </button>
             </nav>
 
